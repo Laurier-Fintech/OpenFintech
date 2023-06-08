11bd50db9b49908e39d052793d4154e9827d1139
harrisiva <harrishiv6@gmail.com>
2023-06-08 15:16:24 -0400

    Updated logger statements in the disconnect funciton

--- OpenFintech/FinMongo.py
+++ OpenFintech/FinMongo.py
@@ -15,32 +15,29 @@ class FinMongo:
         try:
             self.client = MongoClient(host) # host connection string
             self.client.server_info()  # force connection request for testing 
-            self.logger.info("MongoDB Connection Successful.")
-        
         except errors.ConnectionFailure: 
             self.logger.exception("Could not connect to MongoDB (Error: Connection Failure).")   
-
-    # Seperate method for connecting (possible server resource management)?
-    def connect(self):
-        pass
-
-    def disconnect(self):
-        if self.client: self.client.close()
-
-    def __str__(self): # Print the status (connection overview) and other information
+        else:
+            self.logger.info("MongoDB Connection Successful.")
+            
         return
 
+    def connect(self): #NOTE: For handling multiple connections
+        return
 
-# Database
-# contains a list of "collections" objects
-class database:
-    
-    pass
+    def disconnect(self):
+        if self.client: 
+            try:
+                self.client.close()
+            except: self.logger.error("Failed to close the connection.")
+            else: self.logger.info("Sucessfully closed the connection.")
+        return
 
-# Mongo Collection (Table) --> User, etc...
-# collection.insert_doc() 
-class collection:
-    pass
+    def __str__(self): # Print the status (connection overview) and other information
+        sample_info = {'version': '6.0.6', 'gitVersion': '26b4851a412cc8b9b4a18cdb6cd0f9f642e06aa7', 'modules': ['enterprise'], 'allocator': 'tcmalloc', 'javascriptEngine': 'mozjs', 'sysInfo': 'deprecated', 'versionArray': [6, 0, 6, 0], 'bits': 64, 'debug': False, 'maxBsonObjectSize': 16777216, 'storageEngines': ['devnull', 'ephemeralForTest', 'inMemory', 'queryable_wt', 'wiredTiger'], 'ok': 1.0, '$clusterTime': {'clusterTime': Timestamp(1686251487, 4), 'signature': {'hash': b'\x0b\xcc*\xc1\x81\xd9\xebv\x06\xcc\xc05\x9e+\t\xfe&\x1d\xab)', 'keyId': 7204913445559336962}}, 'operationTime': Timestamp(1686251487, 4)}
+        # TODO: Take the sample info and create a string with the important sections for the user
+        return str(self.client.server_info())
+
 
 if __name__ == "__main__": 
     # client = MongoClient(connection_string)
@@ -48,5 +45,5 @@ if __name__ == "__main__":
     # dbs = client.list_database_names()
     # print(dbs)
 
-    mongo_client = Mongo_Client("mongodb+srv://OpenFintech:yT6KHkhVcvHQ42AX@cluster0.lvkyalc.mongodb.net/?retryWrites=true&w=majority")
-
+    handler = FinMongo("mongodb+srv://OpenFintech:yT6KHkhVcvHQ42AX@cluster0.lvkyalc.mongodb.net/?retryWrites=true&w=majority")
+    print(handler)

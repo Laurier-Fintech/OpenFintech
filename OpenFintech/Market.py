56637aa88f74f6b58a5560d2b809e6faf67afa53
harrisiva <harrishiv6@gmail.com>
2023-06-30 19:41:00 -0400

    Updated Market.py's todo (assigning trade CRUD to Ozair and Zahra)

--- OpenFintech/Market.py
+++ OpenFintech/Market.py
@@ -1,9 +1,7 @@
 from .utilities import create_logger
 from .FinMongo import FinMongo
 # TODO:
-# Position CRUD Functions
 # Trade CRUD Functions
-# NOTE: (Required) Integrate FinData (Alphavantage Wrapper Package) into Market to get the required Trade and Position Data. The Market package should also be able to return this data when requested.
 # Implement __str__ to provide an overview of the market
 
 class Market: # Provides simulated backtesting and real-time testing functionalities.
@@ -25,32 +23,26 @@ class Market: # Provides simulated backtesting and real-time testing functionali
         self.trades = self.database["trades"]
         return
 
-    # Realtime (Provides simulated running functionality)
-    def open_position(self):
-        return
-    
-    def close_position(self):
-        return
-    
-    def update_position(self):
-        return
-    
-    def view_position(self):
-        return
-
-    # Backtesting 
+    # TODO: Backtesting (NOTE: Use the ERD and other CRUD functions for reference. Keep default parameters in mind. Feel free to create static methods if you need)
     def create_trade(self):
         return
-    
     def read_trade(self):
         return
-    
     def update_tade(self):
         return
-    
     def delete_trade(self):
         return
     
+    # Realtime (Provides simulated running functionality) NOTE: Ignore these for now
+    def open_position(self):
+        return
+    def close_position(self):
+        return
+    def update_position(self):
+        return
+    def view_position(self):
+        return
+
     # Close database and cleanup
     def close(self):
         if self.inmemory==True: self.mongo.disconnect()
@@ -59,25 +51,3 @@ class Market: # Provides simulated backtesting and real-time testing functionali
     # For providing an overview of the market
     def __str__(self):
         return
-
-
-if __name__=='__main__':
-    sample_trade = {
-        "date_created":None, 
-        "trade_dt":None,
-        # Composite primary key for identifying this document (for future reference)
-        "user_id":None, 
-        "config_id":None,
-        # For identifying the trade and its related session
-        "session_id":None, 
-        "trade_id":None, 
-        # Trade information
-        "ticker":None, 
-        "type":None, 
-        "price":None, 
-        "quantity":None, 
-        "total":None
-    }
-
-    market = Market()
-    market.close()

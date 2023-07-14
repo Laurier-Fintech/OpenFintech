c50565c2b8c1762c0924ca4527fb5f618fc2ac58
harrisiva <harrishiv6@gmail.com>
2023-07-14 16:41:13 -0400

    created FinSQL which uses MySQL (AWS connection) and SQLite3

---
+++ OpenFintech/FinSQL.py
@@ -0,0 +1,49 @@
+import os 
+from dotenv import load_dotenv
+import mysql.connector
+import sqlite3
+
+
+class FinSQL:
+    
+    def __init__(self, host:str=None, user:str=None, password:str=None):
+        self.inmemory = False
+        if host!=None:
+            if user==None or password==None: raise Exception("username and password cannot be none if the host is provided to establish a MySQL connection.")
+            self.conn = mysql.connector.connect(
+                host=host,
+                user=user,
+                password=password
+            )
+        else:
+            self.conn = sqlite3.connect(":memory:")
+            self.inmemory=True
+        self.curr = self.conn.cursor()
+        return
+    
+    def disconnect(self)->bool: 
+        success = True
+        try:
+            self.curr.close()
+            self.conn.disconnect()
+        except: success=False
+        return success
+
+    def __str__(self)->str:
+        if not self.inmemory: 
+            tables = "Tables:\n"
+            self.curr.execute("SHOW DATABASES")
+        else: 
+            tables = response = f"Connected to in-memory db: {self.conn}"
+        for tableName in self.curr: tables+= f"\t{tableName}\n"
+        return tables
+
+
+load_dotenv()
+SQL_USER = os.getenv('MYSQL_USER')
+SQL_PASS = os.getenv('MYSQL_PASS') 
+host = "openfintech.cbbhaex7aera.us-east-2.rds.amazonaws.com"
+
+handler = FinSQL()
+print(handler)
+handler.disconnect()

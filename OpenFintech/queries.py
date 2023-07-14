420c06a032536298588caab8cc0623e19a4835b9
harrisiva <harrishiv6@gmail.com>
2023-07-14 18:21:46 -0400

    Changed equities schemas desc. from varchar(255) to LONGTEXT to avoid errors from companies descriptions being larger than 255 characters

--- OpenFintech/queries.py
+++ OpenFintech/queries.py
@@ -27,27 +27,38 @@ create_config_table = """CREATE TABLE configs (
         FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
 );"""
 
-create_performance_table = """CREATE TABLE users (
-        user_id int PRIMARY KEY AUTO_INCREMENT,
+create_performance_table = """CREATE TABLE configs ( 
+        config_id int NOT NULL AUTO_INCREMENT,
+        user_id int NOT NULL,
         date_created DATETIME DEFAULT now(),
-        username varchar(255) UNIQUE NOT NULL,
-        email varchar(255) UNIQUE DEFAULT NULL,
-        password varchar(255) DEFAULT NULL,
-        year int DEFAULT 0,
-        major varchar(255) DEFAULT NULL,
-        CHECK (year>=1)
-        );"""
 
-create_setting_table = """CREATE TABLE users (
-        user_id int PRIMARY KEY AUTO_INCREMENT,
+        ma_period_1 int DEFAULT 0,
+        ma_period_2 int DEFAULT 0,
+        ema_period_1 int DEFAULT 0,
+        ema_period_2 int DEFAULT 0,
+        rsi_length int DEFAULT 0,
+        ma_length int DEFAULT 0,
+        
+        PRIMARY KEY (config_id),
+        FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
+);"""
+
+create_setting_table = """CREATE TABLE settings ( 
+        setting_id int NOT NULL AUTO_INCREMENT,
+        user_id int NOT NULL,
+        config_id int NOT NULL,
         date_created DATETIME DEFAULT now(),
-        username varchar(255) UNIQUE NOT NULL,
-        email varchar(255) UNIQUE DEFAULT NULL,
-        password varchar(255) DEFAULT NULL,
-        year int DEFAULT 0,
-        major varchar(255) DEFAULT NULL,
-        CHECK (year>=1)
-        );"""
+
+        stop_loss int DEFAULT 0,
+        ma_period_2 int DEFAULT 0,
+        ema_period_1 int DEFAULT 0,
+        ema_period_2 int DEFAULT 0,
+        rsi_length int DEFAULT 0,
+        ma_length int DEFAULT 0,
+        
+        PRIMARY KEY (config_id),
+        FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
+);"""
 
 create_trade_table = """CREATE TABLE users (
         user_id int PRIMARY KEY AUTO_INCREMENT,
@@ -66,7 +77,7 @@ create_equity_table = """CREATE TABLE equities (
         date_created DATETIME DEFAULT now(),
 
         name varchar(255) DEFAULT NULL,
-        description varchar(255) DEFAULT NULL,
+        description LONGTEXT DEFAULT NULL,
         cik varchar(255) DEFAULT NULL,
 
         country varchar(255) DEFAULT NULL,

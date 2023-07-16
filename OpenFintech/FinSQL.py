ac4e03e759941f6e5984adfdcfd896fbea9970fb
harrisiva <harrishiv6@gmail.com>
2023-07-16 18:47:44 -0400

    Updated __init.py__ for OpenFintech packge

--- OpenFintech/FinSQL.py
+++ OpenFintech/FinSQL.py
@@ -1,7 +1,6 @@
 import os 
 from dotenv import load_dotenv
 import mysql.connector
-import sqlite3
 import queries
 
 
--- OpenFintech/__init__.py
+++ OpenFintech/__init__.py
@@ -1,6 +1,6 @@
 from OpenFintech.utilities import *
 from OpenFintech.FinData import FinData
-from OpenFintech.FinMongo import FinMongo
+from OpenFintech.FinSQL import FinSQL
 from OpenFintech.User import User
 from OpenFintech.Market import Market
 from OpenFintech.Model import Model

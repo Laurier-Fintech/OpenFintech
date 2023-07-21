88cafdb18ba18cdbe1726245f4eb7b4510d0db72
harrisiva <harrishiv6@gmail.com>
2023-07-21 18:33:06 -0400

    Added create config method call along with print statements for terminal use

--- main.py
+++ main.py
@@ -2,29 +2,34 @@ import os
 from dotenv import load_dotenv
 from OpenFintech import MySQL, User, Alphavantage, Market, Model
 
-# Setup the database required for the system
-load_dotenv()
+# Setup the database (and handlers) required for the system
+load_dotenv() # Load ENV variables and set them down below
 SQL_USER = os.getenv('MYSQL_USER')
 SQL_PASS = os.getenv('MYSQL_PASS') 
 ALPHAVANTAGE_KEY = os.getenv('ALPHAVANTAGE_KEY') 
 host = "openfintech.cbbhaex7aera.us-east-2.rds.amazonaws.com" #NOTE: Host address is set to the OpenFintech AWS Server.
+# Initiate all the handlers
 db_handler = MySQL(host=host,user=SQL_USER,password=SQL_PASS,database="main")
 data_handler = Alphavantage(database=db_handler,key=ALPHAVANTAGE_KEY)
 config_handler = Model(database=db_handler)
 market_handler = Market(database=db_handler)
+print("Loaded ENV variables and successfully initiated the DB, API, Config, and Market handlers")
 
 # Set the user as the system requires (to add relational layer to the data and faciliate the automated creation of reports etc. through complex queries)
-user_id = 4
+user_id = 4 # TODO: Update user_handler.create() to return the created entry's ID
 
 # Setup the configuration/model for testing
 ema_1 = 5 # Config variable 1
 ema_2 = 20 # Config variable 2
-
+ma_1,ma_2,rsi_1,rsi_2=0,0,0,0 # Set missing values as zero
+config_values = (user_id, ma_1, ma_2, ema_1, ema_2, rsi_1, rsi_2)
+config_id = config_handler.create(config_values) # Create the config using the handler (and store the ID for creating other entries in other related tables)
+print(f"Created {config_values} configuration with the ID {config_id}")
 
 # Setup the parameters for the setting
 ticker = 'GOOGL'
-interval = 30 #min
-# NOTE: Skipping the start and end date ranges for now
+interval = 30 #mins
+# NOTE: Skipping the start and end date ranges for now as they're not required
 
 
 # Disconnect the database

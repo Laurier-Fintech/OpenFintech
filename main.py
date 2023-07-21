c49bf7ea4a43f77800c6bbf29b039d75f7c54885
harrisiva <harrishiv6@gmail.com>
2023-07-21 18:15:57 -0400

    Finished the setup and testing of the db in main.py

--- main.py
+++ main.py
@@ -2,14 +2,21 @@ import os
 from dotenv import load_dotenv
 from OpenFintech import MySQL, Alphavantage, Market, Model
 
-
+# Setup the database required for the system
 load_dotenv()
 SQL_USER = os.getenv('MYSQL_USER')
 SQL_PASS = os.getenv('MYSQL_PASS') 
-host = "openfintech.cbbhaex7aera.us-east-2.rds.amazonaws.com"
-db_handler = MySQL(host=host,user="admin",password="$5svXm!6NAFIL5U",database="main")
-model_handler = Model(db_handler)
+host = "openfintech.cbbhaex7aera.us-east-2.rds.amazonaws.com" #NOTE: Host address is set to the OpenFintech AWS Server.
+db_handler = MySQL(host=host,user=SQL_USER,password=SQL_PASS,database="main")
 
-success = model_handler.create(('1', 25, 3, 4, 5, 6, 7))
-print(success)
+# Set the user as the system requires (to add relational layer to the data and faciliate the automated creation of reports etc. through complex queries)
+
+
+# Setup the configuration/model for testing
+
+
+# Setup the parameters for the setting
+
+
+# Disconnect the database
 db_handler.disconnect()

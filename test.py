11d05fe90d7b4819e93205a948e66f664731071f
harrisiva <harrishiv6@gmail.com>
2023-07-23 00:45:46 -0400

    Setup test.py for implementing backtest algorithm

--- test.py
+++ test.py
@@ -1,25 +1,12 @@
 import pandas as pd
-# Create unit tests for everything
-# (Using this as a test file for building out the algorithm)
 
 df = pd.read_csv("sample_model_data.csv",index_col=0)
 
 print(df)
 
+#TODO: Implement your understanding of the backtesting algorithm here. 
 for i, r in df.iterrows():
     print(i,r)
-
-
-# NOTE: (OLD, go based of your understanding from todays meeting) Algorithm/Loop to test the configuration 
-# Iterate over the pandas df (that contains the price and indicator data)
-# When a signal for opening is hit (based on the configuration)
-#   Open a position
-#   Add trade to the buffer and add signal:position pair to a list for close checking
-# When a signal for closing is hit (based on the config and positions)
-#   Close the associated open position
-#   Register the trade
-# Clear open positions and prepare trade log
-    # Given the trade and market data, calculate the performance data
-
-    # Return the market, positions, trades, and performance data.
-    # Some of these can be returned optionally.
+    # When you hit signals, use lists for tracking open and closed positions (i.e. their associated trade entries)
+    # Check for stop loss, and take profit on the open positions
+    # When closing positions, calculate and create sample performance entries

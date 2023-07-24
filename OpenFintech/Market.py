f34f684933383b91b726a679a62d6f47ca93ee9f
Harri Siva <76477563+harrisiva@users.noreply.github.com>
2023-07-24 10:51:55 -0400

    Revert "Create trade function"

--- OpenFintech/Market.py
+++ OpenFintech/Market.py
@@ -1,37 +1,15 @@
-from . import queries
-
 # TODO:
 # Trade CRUD Functions
 # Implement __str__ to provide an overview of the market (easier and more useful with MySQL)
 # Implement methods in Market to create performance entires
-
 
 class Market: # Provides simulated backtesting and real-time testing functionalities.
 
     def __init__(self, database=None):
         return
     
-    def createTrade(self, trade_configs: dict)->int:
-        return_values = {"user_id": None, 
-                          "equity_id": None, 
-                          "setting_id": None, 
-                          "date_created": None, 
-                          "type" : None,
-                          "trade_dt": None, 
-                          "price": None, 
-                          "quantity": None,
-                          "total": None}
-        
-        for key in trade_configs.keys():
-            return_values[key] = trade_configs[key]
-        
-        values = set(return_values.keys())
-        
-        self.db_handler.execute(queries.insert_trade_entry, values)
-        query_last = "SELECT LAST_INSERT_ID();"
-        trade_id = self.db_handler.execute(query_last, query=True)[0][0]
-
-        return trade_id
+    def createTrade():
+        return
     
     def readTrade():
         return

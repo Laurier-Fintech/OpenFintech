d16c39ae17177c5de25c314a7c68f20ddfb037ae
harrisiva <harrishiv6@gmail.com>
2023-07-28 15:00:16 -0400

    Fixed bug with profit calculation

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ df = pd.read_csv("sample_model_data.csv",index_col=0)
 df['open_position'] = False
 df['profit'] = 0
 df['signal'] = None
-open_position_price = 0;
+open_position_price = 0
 
 #TODO: Implement your understanding of the backtesting algorithm here. 
 for i, r in df.iloc[1:].iterrows():
@@ -13,19 +13,24 @@ for i, r in df.iloc[1:].iterrows():
     # Check for stop loss, and take profit on the open positions
     # When closing positions, calculate and create sample performance entries
 
-    if df['SMA_10'][i] > df['EMA_20'][i] and df['SMA_10'][i - 1] < df['EMA_20'][i - 1]:
+    if df['SMA_10'][i] > df['EMA_20'][i] and df['SMA_10'][i - 1] < df['EMA_20'][i - 1]: # if a buy signal is hit
+        
         df.at[i, 'signal'] = 'buy'
-        if df.at[i - 1, 'open_position'] == False:
+        if df.at[i - 1, 'open_position'] == False: 
             df.at[i, 'open_position'] = True
             open_position_price = df['4. close'][i]
-            df.at[i, 'profit'] = df['profit'][i - 1]
+        
+        df.at[i, 'profit'] = df['profit'][i - 1]
 
     elif df['SMA_10'][i] < df['EMA_20'][i] and df['SMA_10'][i - 1] > df['EMA_20'][i - 1]:
+        
         df.at[i, 'signal'] = 'sell'
         if df.at[i - 1, 'open_position'] == True:
             df.at[i, 'open_position'] = False
             df.at[i, 'profit'] = df['4. close'][i] - open_position_price + df['profit'][i - 1]
-
+        else:
+            df.at[i, 'profit'] = df.at[i - 1, 'profit']
+        
     else:
         df.at[i, 'open_position'] = df.at[i - 1, 'open_position']
         df.at[i, 'signal'] = 'hold'

db659d90d63850bbda06e1df95d3ce7b3e4a1a22
harrisiva <harrishiv6@gmail.com>
2023-08-05 01:01:12 -0400

    Created daily pd implementation to test visualization and build basic algorithm (and also identify bugs in the system to resolve and update)

--- dump.py
+++ dump.py
@@ -1,15 +1,26 @@
-#NOTE: File used to create and handle simple database stuff during development
-import os 
-from dotenv import load_dotenv
-from OpenFintech import MySQL, User
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+plt.style.use('fivethirtyeight')
+from OpenFintech import Alphavantage
 
-load_dotenv()
-SQL_USER = os.getenv('MYSQL_USER')
-SQL_PASS = os.getenv('MYSQL_PASS') 
-host = "openfintech.cbbhaex7aera.us-east-2.rds.amazonaws.com"
-db_handler = MySQL(host=host,user=SQL_USER,password=SQL_PASS,database="main")
-user_handler = User(db_handler)
+ALPHAVANTAGE_KEY = os.getenv('ALPHAVANTAGE_KEY') 
 
-success = user_handler.create(("David","test@mylaurier.ca","12@test", "3","computer science",),simple=False)
-print(success)
-db_handler.disconnect()
+ticker = "HRL"
+response = Alphavantage._request(url=f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={ticker}&apikey={ALPHAVANTAGE_KEY}")
+response = response["Time Series (Daily)"]
+
+df = pd.DataFrame.from_dict(response, orient="index", dtype=float).iloc[::-1]
+
+# Create the SMA 30
+df["5. SMA5"] = df["4. close"].rolling(window=5).mean()
+df["6. SMA15"] = df["4. close"].rolling(window=15).mean()
+print(df)
+
+plt.figure(figsize=(12.5,4.5))
+plt.plot(df["4. close"], label="Close")
+plt.plot(df["5. SMA5"], label="SMA5")
+plt.plot(df["6. SMA15"], label="SMA15")
+plt.legend(loc ="lower right")
+plt.show()
+

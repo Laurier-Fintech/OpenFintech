ee3c25bb261aa50bcd1130634dd520e3fd243056
harrisiva <harrishiv6@gmail.com>
2023-08-19 04:11:52 -0400

    Updated setup.py for updated PyPi launch with docs and requirements to ensure that the user does not miss out on requires

--- setup.py
+++ setup.py
@@ -1,7 +1,23 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="OpenFintech",
-    version="0.1.0",
+    version="0.1.1",
+    author = 'Laurier Fintech',
+    author_email = 'team@wlufintech.com',
+    url = 'https://github.com/Laurier-Fintech/OpenFintech',
     packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+          'requests',
+          'pandas',
+          'numpy',
+          'mysql-connector-python',
+          'python-dotenv',
+    ]
 )

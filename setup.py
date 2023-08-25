58998a6df33321109045ac52c3489e1d46a7a514
harrisiva <harrishiv6@gmail.com>
2023-08-25 16:17:55 -0400

    Updated version in PyPI

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="OpenFintech",
-    version="0.1.5",
+    version="0.1.6",
     author = 'Laurier Fintech',
     author_email = 'team@wlufintech.com',
     url = 'https://github.com/Laurier-Fintech/OpenFintech',

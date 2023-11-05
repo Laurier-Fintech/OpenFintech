bd30ab6156bf80fd4dee4342a60f0300964497b2
Sam120204 <75916625+Sam120204@users.noreply.github.com>
2023-11-05 13:22:56 -0500

    Candle Class

--- OpenFintech/Data.py
+++ OpenFintech/Data.py
@@ -1,5 +1,56 @@
 from collections import deque 
+from collections import deque
+from typing import Deque
 
+class Candle:
+    def __init__(self, open_price: float, high: float, low: float, close_price: float, volume: int, datetime: str, durationSeconds: float):
+        self.open_price = open_price
+        self.high = high
+        self.low = low
+        self.close_price = close_price
+        self.volume = volume
+        self.datetime = datetime
+        self.duration_seconds = duration_seconds
+
+class CandleContainer:
+    def __init__(self):
+        self.candleList: Deque[Candle] = deque()
+
+    def count(self) -> int:
+        return len(self.candleList)
+    
+    def __getitem__(self, index: int) -> Candle:
+        return self.candleList[index]
+
+    def front(self) -> Candle:
+        if not self.candleList:
+            raise IndexError("The CandleContainer is empty.")
+        return self.candleList[0]
+
+    def back(self) -> Candle:
+        if not self.candleList:
+            raise IndexError("The CandleContainer is empty.")
+        return self.candleList[-1]
+
+    def pushFront(self, candle: Candle):
+        self.candleList.appendleft(candle)
+
+    def pushBack(self, candle: Candle):
+        self.candleList.append(candle)
+
+    def popFront(self) -> Candle:
+        if not self.candleList:
+            raise IndexError("The CandleContainer is empty.")
+        return self.candleList.popleft()
+
+    def popBack(self) -> Candle:
+        if not self.candleList:
+            raise IndexError("The CandleContainer is empty.")
+        return self.candleList.pop()
+
+    def clear(self):
+        self.candleList.clear()
+        
 # NOTE: The data structures below are i.e. a replacement to having the database
 class Candle:
     def __init__(self):

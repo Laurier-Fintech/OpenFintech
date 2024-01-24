090d3cb37e58c9804430a4db7183430ebe6f774f
harrisiva <harrishiv6@gmail.com>
2024-01-24 14:19:44 -0500

    Updated package version number:

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="OpenFintech",
-    version="0.1.9",
+    version="0.2.0",
     author = 'Laurier Fintech',
     author_email = 'team@wlufintech.com',
     url = 'https://github.com/Laurier-Fintech/OpenFintech',
